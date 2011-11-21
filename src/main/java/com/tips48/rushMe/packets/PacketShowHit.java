6814bb919233c7e1fcddc7c680a9c53c5d992ec6
tips48 <amatzko48@gmail.com>
2011-11-21 17:07:27 -0500

    Packet ShowHit

---
+++ src/main/java/com/tips48/rushMe/packets/PacketShowHit.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of RushMe.
+ *
+ * RushMe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RushMe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.tips48.rushMe.packets;
+
+import com.tips48.rushMe.custom.GUI.MainHUD;
+import com.tips48.rushMe.custom.GUI.SpoutGUI;
+
+import org.getspout.spoutapi.io.*;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+public class PacketShowHit extends AddonPacket {
+
+	@Override
+	public void read(SpoutInputStream stream) {
+	}
+
+	@Override
+	public void run(SpoutPlayer sp) {
+		MainHUD hud = SpoutGUI.getHudOf(sp);
+		if (hud != null) {
+			hud.showHit();
+		}
+	}
+
+	@Override
+	public void write(SpoutOutputStream stream) {
+	}
+
+}
